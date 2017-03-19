76e9ca6e046541de377fb3e23efa7e02dc41e5f2
jennypenfield <jipenfield11@gmail.com>
2017-03-19 18:31:55 -0500

    committed hw for fn that computes difference betw two numbers

---
+++ difference.js
@@ -0,0 +1,11 @@
+//  Define a function called difference, with two parameters, that returns the difference between two values.
+//
+// define the function here
+//
+var result = difference(385, 142)
+//
+console.log(result)
+
+function difference (num1, num2) {
+  return num1 - num2
+}
